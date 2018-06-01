573af177156389aeae5aacaf3bbd626a9de715d4
Gustavo Pistore <thetunner@hotmail.com>
2018-06-01 13:07:02 -0300

    inicial

---
+++ cmake-build-debug/CMakeFiles/8puzzle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8puzzle.dir/main.cpp.obj"
+  "8puzzle.pdb"
+  "8puzzle.exe"
+  "8puzzle.exe.manifest"
+  "lib8puzzle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8puzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
