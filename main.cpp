6c70d1803796d2faa37c0ef2e32bb7c5e1034bbd
Gustavo Pistore <thetunner@hotmail.com>
2018-06-02 22:56:42 -0300

    primeira versão, não funcional

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+int cont=0;
+int tab[3][3]={{8,0,6},
+               {5,4,7},
+               {2,3,1}};
+void mostra()
+{
+    int i,j;
+    printf("Solução %d:\n\n",++cont);
+    for (i=0;i<3;i++){
+        for (j=0;j<3;j++)
+            printf("%2d ",tab[i][j]);
+        printf("\n\n");
+    }
+    printf("\n\n");
+}
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void back(int nivel, int lin, int col)
+{
+    if (lin<0 || lin>2 || col<0 || col>2) return;
+    if (tab[lin][col]!=0) return;
+    tab[lin][col]=nivel;
+    if (nivel==9)
+    {
+        mostra();
+        tab[lin][col]=0;
+        return;
+    }
+    back(nivel+1,lin-1,col-1);
+    back(nivel+1,lin-2,col+1);
+    back(nivel+1,lin-1,col-2);
+    back(nivel+1,lin-1,col+2);
+    back(nivel+1,lin+1,col-2);
+    back(nivel+1,lin+1,col+2);
+    back(nivel+1,lin+2,col-1);
+    back(nivel+1,lin+2,col+1);
+    tab[lin][col]=0;
+}
+int main()
+{
+    back(1,0,0);
+    system("pause");
 }
